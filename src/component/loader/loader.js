705071a4e00f94c0fd347925e4d7dfaa8066c179
Anuj Kumar Sharma <niranjanamor@gmail.com>
2023-05-31 19:16:52 +0530

    loader added

---
+++ src/component/loader/loader.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Loader.css";
+const Loader = () => {
+  return (
+    <div id="loader-wrapper">
+      <div id="loader5" className="loaderbox">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
